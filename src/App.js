3303a14e7a9939b6c7b55979582a87629ed31d35
Defa <defa.fi@students.amikom.ac.id>
2022-03-24 21:44:10 +0700

    Change element to components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-
-import './App.css';
+import "./App.css";
 import { dataTest } from "./data";
-import React, {useEffect } from 'react';
+import React, { useEffect } from "react";
+import { Stocks } from "./components";
 
 function App() {
-
   useEffect(() => {
     // Update the document title using the browser API
     console.log(dataTest);
   });
 
-  return (
- 
-    <div className="App">
-    <div className="main">
-      <div className="col-image">
-        <img src="https://images.unsplash.com/photo-1646826795556-7c6c5462bb78?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=958&q=80"/>
-        <h2>Be Careful On The Way</h2>
-        <h4>Human</h4>
-        <p>Sincere</p>
-        <button className="btn2">Select</button>
-      </div>
-    </div>
-   
-    </div>
-  );
+  return <Stocks />;
 }
 
 export default App;
---
+++ src/components.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+export const Stocks = () => {
+  return (
+    <>
+      <div className="App">
+        <div className="main">
+          <div className="col-image">
+            <img src="https://images.unsplash.com/photo-1646826795556-7c6c5462bb78?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=958&q=80" />
+            <h2>Be Careful On The Way</h2>
+            <h4>Human</h4>
+            <p>Sincere</p>
+            <button className="btn2">Select</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
