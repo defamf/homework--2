600ab0b22238cf9555a7495e236aa6b79fcd0077
Defa <defa.fi@students.amikom.ac.id>
2022-03-28 08:29:27 +0700

    Loop component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import "./App.css";
 import { dataTest } from "./data";
-import React, { useEffect } from "react";
+import React from "react";
 import { Stocks } from "./components";
 
+class Music extends React.Component {
+  render() {
+    const listItem = dataTest.map((dataTest) => (
+      <li key={dataTest.id}> {dataTest.name} </li>
+    ));
+    return (
+      <div className="baru">
+        <Stocks />
+        <ul>{listItem}</ul>
+      </div>
+    );
+  }
+}
+/*}
 function App() {
   useEffect(() => {
     // Update the document title using the browser API
@@ -10,6 +24,6 @@ function App() {
   });
 
   return <Stocks />;
-}
+}*/
 
-export default App;
+export default Music;
